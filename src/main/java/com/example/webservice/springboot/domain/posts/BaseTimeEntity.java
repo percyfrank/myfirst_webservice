3f56e848d6cf5fd251cb6cbfccc901c332baaf4f
ohsuk kwon <percykwon@gmail.com>
2022-07-23 08:23:10 +0900

    gitignore 등록

---
+++ src/main/java/com/example/webservice/springboot/domain/posts/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.example.webservice.springboot.domain.posts;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+// JPA Entity 클래스들이 해당 클래스를 상속할 경우 필드(createdDate,modifiedDate) 또한 컬럼으로 인식
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) // 해당 클래스에 Auditing 기능 포함
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate // 조회한 Entity의 값을 변경할 때 시간이 자동 저장
+    private LocalDateTime modifiedDate;
+}
--- src/main/java/com/example/webservice/springboot/service/posts/PostsService.java
+++ src/main/java/com/example/webservice/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.example.webservice.springboot.service.posts;
 
+import com.example.webservice.springboot.domain.posts.Posts;
 import com.example.webservice.springboot.domain.posts.PostsRepository;
+import com.example.webservice.springboot.web.dto.PostsResponseDto;
 import com.example.webservice.springboot.web.dto.PostsSaveRequestDto;
+import com.example.webservice.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,8 +15,28 @@ public class PostsService {
 
     private final PostsRepository postsRepository;
 
+    //회원 등록
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    // 회원 수정
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    //회원 조회
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
